059c1a81b5f8f84c6feab98fa4bb9aba2a429e5d
Benjamin Ormeño <benja.ormeno.lr@gmail.com>
2024-04-22 16:39:50 -0300

    agregue estilos y tmb Apis a archivo main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+const listaPokemon = document.querySelector('#listaPokemon');
+let URL = "https://pokeapi.co/api/v2/pokemon/";
+
+for (let i = 1; i <= 151; i++){
+    fetch(URL + i)
+        .then((response) => response.json())
+        .then(data => mostrarPokemon(data))
+};
+
+function mostrarPokemon(poke) {
+    const div = document.createElement('div');
+    div.classList.add('pokemon');
+    div.innerHTML = `
+        <p class="pokemon-id-back">#${poke.id}</p>
+        <div class="pokemon-image">
+            <img src="${poke.sprites.other['official-artwork'].front_default}" alt="${poke.id}">
+        </div>
+        <div class="pokemon-info">
+            <div class="nombre-contenedor">
+                <p class="pokemon-id">#${poke.id}</p>
+                <h2 class="pokemon-nombre">${poke.name}</h2>
+            </div>
+            <div class="pokemon-tipos">
+                <p class="electric tipo">ELECTRIC</p>
+                <p class="fighting tipo">FIGHTING</p>
+            </div>
+            <div class="pokemon-stats">
+                <p class="stat">${poke.height}m</p>
+                <p class="stat">${poke.weight}kg</p>
+            </div>
+        </div>
+    `;
+    listaPokemon.append(div);
+};
